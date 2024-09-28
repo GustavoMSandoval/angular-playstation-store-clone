350781f29bd868d5a7aff29098050ae162392e05
GustavoMSandoval <gmsandoval2005@gmail.com>
2024-09-28 11:14:39 -0300

    Pipes

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,9 +1,11 @@
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TransformadorPipe } from "../../../transformador.pipe";
 
 @Component({
   selector: 'app-card-pricing',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, TitleCasePipe, TransformadorPipe],
   templateUrl: './card-pricing.component.html',
   styleUrl: './card-pricing.component.scss'
 })
